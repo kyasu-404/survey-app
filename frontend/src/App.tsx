c062a31195103df97f97a8e200aa244bd996e721
kyasu-404 <kyasu404@gmail.com>
2026-03-24 13:51:29 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Form from "./pages/Form";
 
-<BrowserRouter>
-  <Routes>
-    <Route path="/form/:id" element={<Form />} />
-  </Routes>
-</BrowserRouter>
+import Builder from "./pages/Builder";
+import Form from "./pages/Form";
+import FormsList from "./pages/FormsList";
+import AdminUsers from "./pages/AdminUsers";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FormsList />} />
+        <Route path="/builder" element={<Builder />} />
+        <Route path="/form/:id" element={<Form />} />
+        <Route path="/admin/users" element={<AdminUsers />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
