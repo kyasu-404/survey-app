93b873782d11b20d10a0885feae35a12e7c3ec3c
kyasu-404 <kyasu404@gmail.com>
2026-03-24 13:26:41 +0300

    Update Form.tsx

--- frontend/src/pages/Form.tsx
+++ frontend/src/pages/Form.tsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
 import { supabase } from "../lib/supabase";
+import { useParams } from "react-router-dom";
 
 export default function Form({ id }) {
+  const { id } = useParams();
   const [survey, setSurvey] = useState(null);
 
   useEffect(() => {
