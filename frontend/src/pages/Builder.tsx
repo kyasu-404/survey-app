40fd29b0bf86c35b17ebecd0c69a59d3cad18ff7
kyasu-404 <kyasu404@gmail.com>
2026-03-24 16:49:06 +0300

    Update Builder.tsx

--- frontend/src/pages/Builder.tsx
+++ frontend/src/pages/Builder.tsx
@@ -9,12 +9,13 @@ export default function Builder() {
   creator.saveSurveyFunc = async (_, callback) => {
     const schema = creator.JSON;
 
-    await supabase.from("forms").insert({
-      title: "Новая форма",
-      form_type: "anketa",
-      form_reason: "plan",
-      schema
-    });
+  await supabase.from("forms").insert({
+    title: "Новая форма",
+    form_type: "anketa",
+    form_reason: "plan",
+    schema,
+    author_id: (await supabase.auth.getUser()).data.user.id
+  });
 
     callback(_, true);
   };
