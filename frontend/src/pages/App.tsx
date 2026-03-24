860061d908326401060beaa3c037d47bba02bbbc
kyasu-404 <kyasu404@gmail.com>
2026-03-24 13:38:45 +0300

    Delete frontend/src/pages/App.tsx

--- frontend/src/pages/App.tsx
+++
@@ -1,8 +0,0 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Form from "./pages/Form";
-
-<BrowserRouter>
-  <Routes>
-    <Route path="/form/:id" element={<Form />} />
-  </Routes>
-</BrowserRouter>
